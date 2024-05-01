d2cb6f1850ade1e9a15d4895292218c8a7a244c1
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-05-02 03:38:15 +0530

    anagram.js sol

--- assignments/01-js/easy/anagram.js
+++ assignments/01-js/easy/anagram.js
@@ -5,7 +5,30 @@
 */
 
 function isAnagram(str1, str2) {
+  let n = str1.length;
+  let m = str2.length;
 
+  if (n !== m) return false;
+
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let i=0; i<n; i++) {
+    obj1[str1[i]] = (obj1[str1[i]] || 0) + 1;
+    obj2[str2[i]] = (obj2[str2[i]] || 0) + 1;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
